28803f2b7e0904dd6f8e86968770141752506ed8
saputrakmdn <saputra.kmdn@gmail.com>
2022-09-02 17:25:38 +0700

    local js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <script src="{{asset('template/js/font-awesome/js/all.js')}}" crossorigin="anonymous"></script>
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
@@ -293,10 +293,10 @@
     </div>
 </div>
 <!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('vendor/jquery/jquery.js')}}"></script>
+<script src="{{asset('template/js/bootstrap.bundle.min.js')}}"></script>
 <!-- Core theme JS-->
 <script src="{{asset('template/js/scripts.js')}}"></script>
-<script src="{{asset('vendor/jquery/jquery.js')}}"></script>
 <script>
     $('#klaster-1').submit(function (e) {
         e.preventDefault();
